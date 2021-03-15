51e9040ab93f363660888694995fbacadf996f54
Michael Levan <mlevan1992@gmail.com>
2021-03-15 09:59:04 -0400

    Python test

---
+++ Day-3/Python/tftest/test_rg.py
@@ -0,0 +1,26 @@
+import os
+import time
+import sys
+
+
+def terraform_init(path):
+  os.system(f'cd {path} && terraform init')
+
+def terraform_plan(path):
+  os.system(f'cd {path} && terraform plan')
+
+def terraform_apply(path):
+  os.system(f'cd {path} && terraform apply -auto-approve')
+
+def terraform_destroy(path):
+  os.system(f'cd {path} && terraform destroy -auto-approve')
+
+
+path = sys.argv[1]
+
+
+if __name__ == '__main__':
+  terraform_init(path)
+  terraform_plan(path)
+  terraform_apply(path)
+  terraform_destroy(path)
